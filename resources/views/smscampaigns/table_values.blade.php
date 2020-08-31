eaa9adb247ed0fd463141fe4098fceb34472e158
j2cey <jud10parfait@gmail.com>
2020-08-31 15:41:08 +0000

    design display campaign status badge

--- resources/views/smscampaigns/table_values.blade.php
+++ resources/views/smscampaigns/table_values.blade.php
@@ -4,13 +4,13 @@
 <td>{{ $currval->message }}</td>
 <td>
     @if ($currval->status->code == 0)
-        <span class="badge badge-success">
+        <span class="badge badge-danger">
     @elseif($currval->status->code == 1)
-                <span class="badge badge-light">
+                <span class="badge badge-secondary">
     @elseif($currval->status->code == 2)
-                        <span class="badge badge-secondary">
+                        <span class="badge badge-warning">
     @else
-                                <span class="badge badge-danger">
+                                <span class="badge badge-success">
     @endif
                                     {{ $currval->status->titre }}</span>
 </td>
