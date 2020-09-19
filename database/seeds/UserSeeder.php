569924f1c0a7a54afbacfaeac098fec43dfdc29d
j2cey <jud10parfait@gmail.com>
2020-09-19 21:07:07 +0000

    creation des comptes pour la DV

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,5 +14,8 @@ class UserSeeder extends Seeder
     {
         $user = User::create(['name' => "rootdev",'email' => "rootdev@gestockdsi.com",'password' => bcrypt('Gestock@Pw0rd')]);
         $user = User::create(['name' => "admin",'email' => "admin@gt.com",'password' => bcrypt('admin123')]);
+        $user = User::create(['name' => "DV 01",'email' => "dv1@gt.com",'password' => bcrypt('dvlibertis123')]);
+        $user = User::create(['name' => "DV 02",'email' => "dv2@gt.com",'password' => bcrypt('dvlibertis123')]);
+        $user = User::create(['name' => "DV 03",'email' => "dv3@gt.com",'password' => bcrypt('dvlibertis123')]);
     }
 }
